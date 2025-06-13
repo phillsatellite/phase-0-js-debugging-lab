09b387e23c3ec331865016319bfa995f363bd83a
phillsatellite <pprints12@gmail.com>
2025-06-13 02:22:40 -0400

    debugging lab attmpt 1

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,18 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+       
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+    debugger;
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
-
+    debugger;
     return totalPrice;
 }
 
-
+console.log(calculateDiscountedPrice(15, 5));
 
 module.exports = calculateDiscountedPrice;
